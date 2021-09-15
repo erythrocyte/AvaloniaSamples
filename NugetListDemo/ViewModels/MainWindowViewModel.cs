a09fb08aa8fa8517ea9673ead5306ef947ac53f1
marseille <hamidullinmr@actual-technology.ru>
2021-09-15 14:23:11 +0300

    main menu2

--- NugetListDemo/ViewModels/MainWindowViewModel.cs
+++ NugetListDemo/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,16 @@
-﻿namespace NugetListDemo.ViewModels
+﻿using System.Reflection;
+
+namespace NugetListDemo.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         public MainWindowViewModel()
         {
+            var avaVersion = Assembly.GetAssembly(typeof(Avalonia.AvaloniaLocator))?.GetName()?.Version?.ToString() ?? string.Empty;
+            Title = $"Nuget List Demo [avalonia: { avaVersion }]";
         }
+
+        public string Title {get;}
     }
+
 }
