fd0455ff4c2d2f2f4d930b9ff0a1d7917d908c33
erythrocyte <marseille.tatar@yahoo.com>
2022-09-06 18:54:30 +0300

    first lesson completed

--- AngelSixTutor/AvaloniaBasics/Views/MainWindow.axaml.cs
+++ AngelSixTutor/AvaloniaBasics/Views/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
+using System.Diagnostics;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Microsoft.CodeAnalysis.Text;
 
 namespace AvaloniaBasics.Views
 {
@@ -7,6 +11,32 @@ namespace AvaloniaBasics.Views
         public MainWindow()
         {
             InitializeComponent();
+            Debug.WriteLine("Test output");
+        }
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"{this.DescriptionText.Text}");
+        }
+
+        private void ResetButton_OnClick(object? sender, RoutedEventArgs e)
+        {
+            this.WeldCheckbox.IsChecked = this.AssemblyCheckbox.IsChecked = this.PlasmaCheckbox.IsChecked =
+                this.LaserCheckbox.IsChecked = this.PurchaseCheckbox.IsChecked =
+                    this.LatheCheckbox.IsChecked = this.DrillCheckbox.IsChecked = this.FoldCheckbox.IsChecked =
+                        this.RollCheckbox.IsChecked = this.SawCheckbox.IsChecked = false;
+        }
+
+        private void Checkbox_OnChecked(object? sender, RoutedEventArgs e)
+        {
+            if (sender is CheckBox ch)
+                LengthText.Text += ch.Content;
+        }
+        
+        private void SupplierNameText_OnPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+        {
+            if (MassText != null)
+                this.MassText.Text = SupplierNameText.Text;
         }
     }
 }
