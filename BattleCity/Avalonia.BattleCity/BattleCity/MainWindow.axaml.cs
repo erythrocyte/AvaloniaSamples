a3a495030650acdf257f1d1dec363298851add86
erythrocyte <marseille.tatar@yahoo.com>
2022-09-07 22:08:35 +0300

    w

---
+++ BattleCity/Avalonia.BattleCity/BattleCity/MainWindow.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+using BattleCity.Models;
+
+namespace BattleCity;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        var field = new GameField();
+        var game = new Game(field);
+        game.Start();
+    }
+
+    protected override void OnKeyDown(KeyEventArgs e)
+    {
+        Keyboard.Keys.Add(e.Key);
+        base.OnKeyDown(e);
+    }
+
+    protected override void OnKeyUp(KeyEventArgs e)
+    {
+        Keyboard.Keys.Remove(e.Key);
+        base.OnKeyUp(e);
+    }
+}
