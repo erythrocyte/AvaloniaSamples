8bb2852a2dee3a544553ba45a1e604a6503a921d
erythrocyte <marseille.tatar@yahoo.com>
2022-09-07 19:39:39 +0300

    snapshot2

---
+++ AngelSixTutor/AvaloniaTreeView/MainWindow.axaml.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Avalonia.Controls;
+
+namespace AvaloniaTreeView
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TopLevel_OnOpened(object? sender, EventArgs e)
+        {
+            var items = new List<TreeViewItem>();
+            int max_count = 3, count = 0;
+            foreach (var drive  in Directory.GetLogicalDrives())
+            {
+                if (count > max_count)
+                    break;
+                
+                var item = new TreeViewItem();
+                item.Header = drive;
+
+                items.Add(item);
+
+                count++;
+            }
+            
+            FolderView.Items = items;
+        }
+    }
+}
