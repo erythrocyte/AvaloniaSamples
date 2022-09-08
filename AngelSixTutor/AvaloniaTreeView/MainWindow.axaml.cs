25573c72eb8f35e858fed7e1fb4f77851bfeb2db
erythrocyte <marseille.tatar@yahoo.com>
2022-09-08 21:03:48 +0300

    impossible to add icon to treeviewitem

--- AngelSixTutor/AvaloniaTreeView/MainWindow.axaml.cs
+++ AngelSixTutor/AvaloniaTreeView/MainWindow.axaml.cs
@@ -3,33 +3,41 @@ using System.Collections.Generic;
 using System.IO;
 using Avalonia.Controls;
 
-namespace AvaloniaTreeView
+namespace AvaloniaTreeView;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    private void TopLevel_OnOpened(object? sender, EventArgs e)
+    {
+        var items = new List<TreeViewItem>();
+        int max_count = 3, count = 0;
+        foreach (var drive in Directory.GetLogicalDrives())
         {
-            InitializeComponent();
+            if (count > max_count)
+                break;
+
+            var item = new TreeViewItem();
+            item.Header = drive;
+            item.Name = drive;
+            // Template = new ControlTemplate
+            // {
+            //     Content = drive
+            // };
+
+            item.Items = new List<TreeViewItem> { null };
+            item.IsExpanded = true;
+
+
+            items.Add(item);
+
+            count++;
         }
 
-        private void TopLevel_OnOpened(object? sender, EventArgs e)
-        {
-            var items = new List<TreeViewItem>();
-            int max_count = 3, count = 0;
-            foreach (var drive  in Directory.GetLogicalDrives())
-            {
-                if (count > max_count)
-                    break;
-                
-                var item = new TreeViewItem();
-                item.Header = drive;
-
-                items.Add(item);
-
-                count++;
-            }
-            
-            FolderView.Items = items;
-        }
+        FolderView.Items = items;
     }
 }
